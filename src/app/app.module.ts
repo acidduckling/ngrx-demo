cacd5833eceb5b8635853f3cfdd6a2633c27663b
CLayton Wakefield <acidduckling@gmail.com>
2018-05-15 12:51:12 +1000

    Simple Reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
-
+import { simpleReducer } from './simple.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ message: simpleReducer })],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
