045d98a59110f991c699904741ef462a89a58e08
CLayton Wakefield <acidduckling@gmail.com>
2018-05-15 17:54:44 +1000

    Improved implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
-
 import { AppComponent } from './app.component';
+import { postReducer } from './reducers/post.reducer';
 import { simpleReducer } from './simple.reducer';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule.forRoot({ message: simpleReducer })],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ post: postReducer, message: simpleReducer })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
