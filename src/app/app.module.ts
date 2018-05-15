39ac0b68e2e24902f470c9fee540176a84aab2c7
CLayton Wakefield <acidduckling@gmail.com>
2018-05-15 18:06:05 +1000

    Add Chrome Redux DevTools Extension Support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,17 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { postReducer } from './reducers/post.reducer';
 import { simpleReducer } from './simple.reducer';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    StoreModule.forRoot({ post: postReducer, message: simpleReducer })
+    StoreModule.forRoot({ post: postReducer, message: simpleReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
